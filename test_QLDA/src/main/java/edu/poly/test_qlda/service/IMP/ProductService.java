e34311c0833c01aa2b66dc8bdd468703b76c8925
caodinh <caococ2002@gmial.com>
2022-07-22 17:38:25 +0700

    Commit By Cao

--- test_QLDA/src/main/java/edu/poly/test_qlda/service/IMP/ProductService.java
+++ test_QLDA/src/main/java/edu/poly/test_qlda/service/IMP/ProductService.java
@@ -1,9 +1,36 @@
 package edu.poly.test_qlda.service.IMP;
 
 
+import edu.poly.test_qlda.entity.ProductEntity;
+import edu.poly.test_qlda.infrastructure.response.ProductResponse;
+import edu.poly.test_qlda.repository.IProductRepository;
 import edu.poly.test_qlda.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService implements IProductService {
+    @Autowired
+    private IProductRepository productRepository;
 
+    @Override
+    public String deleteProduct(Long id) {
+        Optional<ProductEntity> entity = productRepository.findById(id);
+        if (!entity.isPresent()) {
+            return "id không tồn tại trong product";
+        } else {
+            productRepository.delete(entity.get());
+            return "xóa thành công";
+        }
+    }
 
+    @Override
+    public List<ProductResponse> findAllProduct(Pageable pageable) {
+        List<ProductResponse> list = productRepository.findAllProduct(pageable);
+        return list;
+    }
 }
--- test_QLDA/src/main/java/edu/poly/test_qlda/service/IProductService.java
+++ test_QLDA/src/main/java/edu/poly/test_qlda/service/IProductService.java
@@ -1,13 +1,12 @@
 package edu.poly.test_qlda.service;
 
-import edu.poly.test_qlda.infrastructure.request.SubCategoryRequest;
 import edu.poly.test_qlda.infrastructure.response.ProductResponse;
-import edu.poly.test_qlda.infrastructure.request.ProductRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IProductService {
-
+    String deleteProduct(Long id);
+    List<ProductResponse> findAllProduct(Pageable pageable);
 
 }
